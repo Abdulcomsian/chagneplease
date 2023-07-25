bffce953e80aebd323cfc09533ed829dc4096c69
nraccrual <nauman.riaz@accrualhub.com>
2023-07-25 12:52:51 +0500

    dashbord card for admin issue resolved

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{Plan , User};
 
 class DashboardController extends Controller
 {
@@ -13,7 +14,11 @@ class DashboardController extends Controller
 
     public function get_admin_dashboard()
     {
-        return view('admin.dashboard');
+        $totalPlans = Plan::count();
+        $totalUsers = User::count();
+        $totalInvestment = number_format(Plan::get()->sum('amount') , 2);
+        $requiredInvestment = number_format(Plan::get()->sum('investment') , 2);
+        return view('admin.dashboard')->with(['totalPlans' => $totalPlans , 'totalUsers' => $totalUsers , 'totalInvestment' => $totalInvestment , 'requiredInvestment' => $requiredInvestment]);
     }
 
     public function get_analyst_dashboard()
